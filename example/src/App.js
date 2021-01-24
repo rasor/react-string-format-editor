efaf4b072103fe31c1ce469bf6d28723042b951d
rasor <soren@rasor.dk>
2021-01-24 15:12:41 +0100

    sample component moved to components/samples

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-string-format-editor'
+import { ExampleComponent } from 'react-string-format-editor'
 
 export default class App extends Component {
   render () {
