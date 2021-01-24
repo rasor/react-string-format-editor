62881a0ea33709a97ab7431485f2a4452908cf4a
rasor <soren@rasor.dk>
2021-01-24 19:41:59 +0100

    'Editor' is not exported!

--- src/components/EditorWrapper/EditorWrapper.tsx
+++ src/components/EditorWrapper/EditorWrapper.tsx
@@ -2,10 +2,16 @@
 
 import * as React from "react";
 
+//import { EditorState } from 'draft-js';
+//import { Editor } from 'react-draft-wysiwyg';
+
+// dist/index.js, dist/index.es.js...
+// [!] Error: 'Editor' is not exported by node_modules\react-draft-wysiwyg\dist\react-draft-wysiwyg.js
+
 const EditorWrapper: React.FC = () => {
 return (
-    <div>
-      abc
+    <div>abc
+      {/* <Editor/> */}
     </div>
   );
 };
